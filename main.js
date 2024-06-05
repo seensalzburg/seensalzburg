6f5325f4bb8bd1f0e833addd0f289f79788591df
AnnaGruber263 <anna.gruber@uibk.ac.at>
2024-06-05 18:00:40 +0200

    files erstellt

---
+++ main.js
@@ -0,0 +1,16 @@
+// Initialisieren der Karte
+let map = L.map('map').setView([47.8095, 13.0550], 8);
+
+// Hinzufügen von Kacheln zur Karte
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+L.control.scale({
+    imperial: false
+}).addTo(map);
