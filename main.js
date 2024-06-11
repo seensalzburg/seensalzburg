934a4c4833323339b33200e47186637ee7248e8c
AnnaGruber263 <anna.gruber@uibk.ac.at>
2024-06-11 16:34:13 +0200

    Seen hinzugefügt, Name im Pop-Up

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+let salzburg = {
+    lat: 47.8095,
+    lng: 13.0550,
+    title: "Salzburg",
+};
+
 // Initialisieren der Karte
-let map = L.map('map').setView([47.8095, 13.0550], 8);
+let map = L.map('map').setView([salzburg.lat, salzburg.lng], 8);
 
 // Hinzufügen von Kacheln zur Karte
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -14,3 +20,44 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.control.scale({
     imperial: false
 }).addTo(map);
+
+let themaLayer = {
+    Seen: L.featureGroup().addTo(map),
+}
+
+//GEOJSON reinladen
+fetch('WIS_Seen/Seen.geojson')
+    .then(response => response.json())
+    .then(data => {
+        // Füge die GeoJSON-Daten zur Karte hinzu
+        L.geoJSON(data,
+            {
+                onEachFeature: onEachFeature
+            }).addTo(map);
+    })
+    .catch(error => console.error('Error loading the GeoJSON data:', error));
+
+
+// Hintergrundlayer
+L.control.layers({
+    "BasemapAT Grau": L.tileLayer.provider("BasemapAT.grau").addTo(map),
+    "BasemapAT Standard": L.tileLayer.provider("BasemapAT.basemap"),
+    "BasemapAT High-DPI": L.tileLayer.provider("BasemapAT.highdpi"),
+    "BasemapAT Gelände": L.tileLayer.provider("BasemapAT.terrain"),
+    "BasemapAT Oberfläche": L.tileLayer.provider("BasemapAT.surface"),
+    "BasemapAT Orthofoto beschriftet": L.layerGroup([
+        L.tileLayer.provider("BasemapAT.orthofoto"),
+        L.tileLayer.provider("BasemapAT.overlay")
+    ]),
+}, {
+    "Seen": themaLayer.Seen,
+})
+    .addTo(map);
+
+//Pop-up
+function onEachFeature(feature, layer) {
+    if (feature.properties && feature.properties.NAME) {
+        layer.bindPopup(feature.properties.NAME);
+    }
+}
+
